505157b025103d51310631d15611e6d6461bdf45
AnaVirtual <derekpicom@live.com>
2016-09-28 14:41:51 +0900

    Added Dialogs and data model to the project.

---
+++ LaughingGame/Dialogs/GreetingDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace LaughingGame.Dialogs
+{
+    [Serializable]
+    public class GreetingDialog : IDialog<object>
+    {
+        public LaughingDataModel laughingData;
+
+        public GreetingDialog(object passedData)
+        {
+            laughingData = passedData as LaughingDataModel;
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> arguments)
+        {
+            var message = await arguments;
+            if(laughingData != null)
+            {
+                await context.PostAsync($"You said: {message.Text} value = {laughingData.value}");
+                context.Wait(new EmotionDialog(laughingData).MessageReceivedAsync);
+            }
+            else
+            {
+                await context.PostAsync($"You said: {message.Text}");
+                context.Wait(MessageReceivedAsync);
+            }
+        }
+    }
+}
---
+++ LaughingGame/Dialogs/ResultDialog.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace LaughingGame.Dialogs
+{
+    [Serializable]
+    public class ResultDialog : IDialog<object>
+    {
+        public LaughingDataModel laughingData;
+        
+        public ResultDialog(object passedData)
+        {
+            laughingData = passedData as LaughingDataModel;
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> arguments)
+        {
+            var message = await arguments;
+            await context.PostAsync($"You said: {message.Text} value = {laughingData.value}");
+            context.Wait(new GreetingDialog(laughingData).MessageReceivedAsync);
+        }
+    }
+}
