a7f2468a28862fc6047fbaf84a2e6ddcc7e0bd78
AnaVirtual <derekpicom@live.com>
2016-09-28 13:12:23 +0900

    Added dialog class.

---
+++ LaughingGame/Dialogs/LaughingGameDialog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+namespace LaughingGame
+{
+    [Serializable]
+    public class LaughingGameDialog : IDialog<object>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        public async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> arguments)
+        {
+            var message = await arguments;
+            await context.PostAsync("You said: " + message.Text);
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
