5b881a592fe66e2024338c6ccb3c7bbb5ddf6e32
Tatsuya <tatsuya.sato@dmp.co.jp>
2016-09-28 12:23:46 +0900

    Sample Source by http://qiita.com/annie/items/c9a8b83234dd869912c9

--- LaughingGame/Controllers/MessagesController.cs
+++ LaughingGame/Controllers/MessagesController.cs
@@ -7,6 +7,10 @@ using System.Web.Http;
 using System.Web.Http.Description;
 using Microsoft.Bot.Connector;
 using Newtonsoft.Json;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using System.Collections.Generic;
+
 
 namespace LaughingGame
 {
@@ -17,25 +21,94 @@ namespace LaughingGame
         /// POST: api/Messages
         /// Receive a message from a user and reply to it
         /// </summary>
-        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
         {
-            if (activity.Type == ActivityTypes.Message)
-            {
-                ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
-                // calculate something for us to return
-                int length = (activity.Text ?? string.Empty).Length;
 
-                // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
-                await connector.Conversations.ReplyToActivityAsync(reply);
-            }
-            else
-            {
-                HandleSystemMessage(activity);
-            }
-            var response = Request.CreateResponse(HttpStatusCode.OK);
-            return response;
+            // メッセージやり取りを行う ConnectorClient を生成
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            // Emotion API の Subscription Key をセット
+            // Emotion API を Call する EmotionServiceClient を生成
+            // 入力値(URL) を元に Emotion API を Call
+            // ※次以降の項目で作成します
+
+            // デフォルトの返答 (初回、または写真判定ができなかったとき))
+            Activity reply = activity.CreateReply("顔の表情を判定します。写真のURLを送ってね。");
+
+            // Call 結果を元に 返答を作成
+            // ※次以降の項目で作成します
+
+            // メッセージ、および http ステータス Accepted(=200) を返答
+            await connector.Conversations.ReplyToActivityAsync(reply);
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
         }
+        public virtual async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+
+            // メッセージやり取りを行う ConnectorClient を生成
+            ConnectorClient connector = new ConnectorClient(new Uri(activity.ServiceUrl));
+
+            // Emotion API の Subscription Key をセット
+            // お持ちの Subscription Key を YOUR_SUBSCRIPTION_KEY 部分にコピーしてください
+            const string emotionApiKey = "e3a71d5333314af38a9f92fc4f0a68bb";
+
+            // Emotion API を Call する EmotionServiceClient を生成
+            EmotionServiceClient emotionServiceClient = new EmotionServiceClient(emotionApiKey);
+            Emotion[] emotionResult = null;
+
+            // 入力値(URL) を元に Emotion API を Call
+            try
+            {
+                emotionResult = await emotionServiceClient.RecognizeAsync(activity.Text);
+            }
+            catch (Exception e)
+            {
+                emotionResult = null;
+            }
+
+            // デフォルトの返答 (初回、または写真判定ができなかったとき))
+            Activity reply = activity.CreateReply("顔の表情を判定します。写真のURLを送ってね。");
+
+            // Call 結果を元に 返答を作成
+            // Call 結果を元に 返答を作成
+            if (emotionResult != null)
+            {
+                //表情スコアを取得
+                Scores emotionScores = emotionResult[0].Scores;
+
+                //取得したスコアを KeyValuePair に代入、スコア数値の大きい順に並び替える
+                IEnumerable<KeyValuePair<string, float>> emotionList = new Dictionary<string, float>()
+        {
+            { "怒ってる", emotionScores.Anger},
+            { "軽蔑してる", emotionScores.Contempt },
+            { "うんざりしてる", emotionScores.Disgust },
+            { "怖がってる", emotionScores.Fear },
+            { "楽しい", emotionScores.Happiness},
+            { "特になし", emotionScores.Neutral},
+            { "悲しい", emotionScores.Sadness },
+            { "驚いてる", emotionScores.Surprise}
+        }
+                .OrderByDescending(kv => kv.Value)
+                .ThenBy(kv => kv.Key)
+                .ToList();
+
+                //スコア数値の一番大きい表情を取得してメッセージにセット
+                KeyValuePair<string, float> topEmotion = emotionList.ElementAt(0);
+                string topEmotionKey = topEmotion.Key;
+                float topEmotionScore = topEmotion.Value;
+
+                reply = activity.CreateReply
+                    (
+                        "顔の表情を判定しました。"
+                        + (int)(topEmotionScore * 100) + "% " + topEmotionKey + "顔だと思うよ。"
+                    );
+            }
+
+            // メッセージ、および http ステータス Accepted(=200) を返答
+            await connector.Conversations.ReplyToActivityAsync(reply);
+            return new HttpResponseMessage(System.Net.HttpStatusCode.Accepted);
+
 
         private Activity HandleSystemMessage(Activity message)
         {
